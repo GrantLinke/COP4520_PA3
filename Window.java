a73e90f333ea6d84042e17edd4b817fb867f29f6
Grant Linke <GLinke123@gmail.com>
2022-04-03 12:51:41 -0400

    Adds find to Window.java

--- Window.java
+++ Window.java
@@ -1,16 +1,39 @@
 public class Window {
-    public PresChain pred;
-    public PresChain curr;
+    public Node pred;
+    public Node curr;
 
-    public Window find(PresChain head, int key) {
-        Window window = new Window();
+    public Window(Node pred, Node curr) {
+        this.pred = pred;
+        this.curr = curr;
+    }
 
-        window.pred = head.contains(head, key);
-        if (window.pred != null) {
-            window.curr = pred.next;
-            return window; // we found it
+    public Window find(Node head, int key) {
+        Node pred = null, curr = null, succ = null;
+        boolean[] marked = { false };
+        boolean snip;
+
+        retry: while (true) {
+            pred = head;
+            curr = pred.next.getReference();
+            while (true) {
+                succ = curr.next.get(marked);
+
+                while (marked[0]) {
+                    snip = pred.next.compareAndSet(curr, succ, false, false);
+                    if (!snip) {
+                        continue retry;
+                    }
+                    curr = succ;
+                    succ = curr.next.get(marked);
+                }
+
+                if (curr.key >= key) { // we have a hit
+                    return new Window(pred, curr);
+                }
+
+                pred = curr; // keep traversing
+                curr = succ;
+            }
         }
-
-        return null; // we couldn't find it
     }
 }
